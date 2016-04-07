50bd3c77ffec0339434b4bd976f5a96349973ec6
anchengjian <anchengjian@gmail.com>
2016-04-07 18:21:39 +0800

    优化404

--- app/components/404/index.js
+++ app/components/404/index.js
@@ -5,7 +5,12 @@ var React = require('react');
 var NotFound = React.createClass({
   render: function (){
     return (
-      <h2 style={{textAlign:'center'}}>404 Not Found</h2>
+      <section className="not-found">
+        <h2 style={{textAlign:'center'}}>404</h2>
+        <p>Not Found</p>
+        <a href='/' className="btn" >回到首页</a>
+        <p>© 2016 anchengjian</p>
+      </section>
     );
   }
 });
