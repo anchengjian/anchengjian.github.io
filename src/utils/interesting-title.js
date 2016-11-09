1c4b57e53eab82cf3c4f3cdb6f5ddf9c4a2d7215
anchengjian <anchengjian@gmail.com>
2016-11-09 21:20:20 +0800

    feat: interesting favicon

--- src/utils/interesting-title.js
+++ src/utils/interesting-title.js
@@ -1,6 +1,14 @@
-export default function interestingTitle(title) {
+export default function (interestingTitle, interestingFavicon) {
+  // origin info
   let originTitle = document.title;
-  let interesting = title || '※18♥禁★电影【在线观看】☆...';
+  let originFavicon = getFaviconEle();
+  let originFaviconHref = originFavicon.href;
+
+  // interesting info
+  interestingTitle = interestingTitle || '※18♥禁★电影【在线观看】☆...';
+  interestingFavicon = interestingFavicon || 'http://cl.hotcool.info/favicon.ico';
+
+  // get prefix support for ...
   let hidden, visibilityChange;
   ['', 'o', 'ms', 'moz', 'webkit'].forEach(prefix => {
     let supportHidden = prefix + (prefix ? 'Hidden' : 'hidden');
@@ -9,21 +17,36 @@ export default function interestingTitle(title) {
     visibilityChange = prefix + 'visibilitychange';
   });
 
+  // not support for return
   if (typeof document.addEventListener === 'undefined' || typeof document[hidden] === 'undefined') return;
 
+  // start interesting
   document.addEventListener(visibilityChange, horseRaceLamp, false);
 
   let timer;
   function horseRaceLamp() {
     if (document[hidden]) {
-      document.title = interesting;
+      document.title = interestingTitle;
+      originFavicon.href = interestingFavicon;
       timer = setInterval(() => {
         let title = document.title;
         document.title = title.substr(1, title.length - 1) + title[0];
       }, 50);
     } else {
       document.title = originTitle;
+      originFavicon.href = originFaviconHref;
       clearInterval(timer);
     }
   }
+}
+
+function getFaviconEle(href) {
+  let ele = document.querySelector('link[rel="shortcut icon"]') || document.querySelector('link[type*=image]');
+  if (ele) return ele;
+  href = href || '/favicon.ico';
+  ele = document.createElement('link');
+  ele.setAttribute('rel', 'shortcut icon');
+  ele.setAttribute('href', href);
+  document.querySelector('head').appendChild(ele);
+  return ele;
 }
